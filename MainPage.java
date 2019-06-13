167924fe994a7733ad27875728104a3c70a43153
a710687582 <51770304+a710687582@users.noreply.github.com>
2019-06-13 11:27:47 +0800

    Update MainPage.java
    修改了主界面的分类名

--- MainPage.java
+++ MainPage.java
@@ -35,9 +35,9 @@ public final class MainPage extends ScannerChoice
 		public static void  mianPage()
 		{
 			System.out.println("***************************\n");
-			System.out.println("\t 1.商品维护\n");
-			System.out.println("\t 2.前台收银\n");
-			System.out.println("\t 3.商品管理\n");
+			System.out.println("\t 1.商品创建\n");
+			System.out.println("\t 2.商品上架\n");
+			System.out.println("\t 3.商品删除\n");
 			System.out.println("***************************");
 			
 			System.out.println("\n请输入选项,或者按0退出.");
